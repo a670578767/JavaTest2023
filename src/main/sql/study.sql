a058939c0432c58d6e9fbfeb24419bbd07cb0616
wangzhiang <wangzhiang@css.com.cn>
2023-02-16 11:59:36 +0800

    20230216 提交SQL外连接练习题

--- src/main/sql/study.sql
+++ src/main/sql/study.sql
@@ -192,3 +192,45 @@ FROM employees e
      departments d
      ON
          e.department_id = d.department_id;
+
+# 8 UNION 合并查询结果 操作符返回两个查询的结果集并集，去除重复纪录  去重
+#   UNION ALL 操作符返回两个查询的结果集并集。对于两个结果集的重复部分，不去重
+
+# 第六章练习题
+#1.显示所有的员工的姓名，部门号和部门名称
+SELECT last_name, e.department_id, department_name
+FROM employees e
+         LEFT JOIN departments d ON e.department_id = d.department_id;
+#2.查询90号部门员工的job_id 和 90号部门的location_id
+SELECT e.job_id, d.location_id
+FROM employees e
+         JOIN departments d on e.department_id = d.department_id
+WHERE d.department_id = 90;
+# 3.选择所有的奖金的员工的last_name，department_name，location_id，city
+SELECT e.last_name, e.commission_pct, d.department_name, d.location_id, l.city
+FROM employees e
+         left JOIN departments d on e.department_id = d.department_id
+         left JOIN locations l on d.location_id = l.location_id
+WHERE commission_pct IS NOT NULL;
+# 4.选择city在Toronto 工作的员工的last_name , job_id ,department_id,department_name
+SELECT e.last_name, e.job_id, d.department_id, d.department_name
+FROM employees e
+         JOIN departments d on e.department_id = d.department_id
+         JOIN locations l on d.location_id = l.location_id
+WHERE l.city = 'Toronto';
+# 5.查询员工所在的部门名称，部门地址，姓名，工作，工资，其中员工在部门的部门名称为“Executive”
+SELECT department_name, street_address, last_name, job_id, salary
+FROM employees e
+         JOIN departments d on d.department_id = e.department_id
+         JOIN locations l on l.location_id = d.location_id
+WHERE department_name = 'Executive';
+
+# 6.选择指定员工的姓名，员工号，以及他的管理者的姓名和员工号，结果类似于下面的格式
+SELECT emp.last_name "employees ", emp.employee_id "Emp#", egr.last_name "manager", egr.employee_id "Mgr#"
+FROM employees emp
+         left join employees egr on emp.manager_id = egr.employee_id;
+# 7.查询哪些部门没有员工
+SELECT d.department_id, e.department_id
+FROM departments d
+         left join employees e on d.department_id = e.department_id
+WHERE e.department_id is null;
